c3cd7753712559b848f4a0c785b3086124493159
Juan Florez <juan.florez@ceiba.com.co>
2018-02-01 09:40:25 -0500

    Se crea el modulo view con sus respectivas vistas y listo para consumir del presenter.

---
+++ parqueadero_ios/parqueadero_ios/RegisterList/Protocols/Protocols.swift
@@ -0,0 +1,79 @@
+//
+//  Protocols.swift
+//  parqueadero_ios
+//
+//  Created by Juan Sebastián Flórez Saavedra - Ceiba Software on 2/1/18.
+//  Copyright © 2018 Juan Sebastián Flórez Saavedra - Ceiba Software. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+protocol RegisterListViewProtocol: class {
+    
+    var presenter: RegisterListPresenterProtocol? { get set }
+    
+    //PRESENTER -> VIEW
+    func showRegister(with register: [Registro])
+    
+    func showError()
+    
+    func showLoading()
+    
+    func hideLoading()
+    
+}
+
+protocol RegisterListPresenterProtocol: class {
+    
+    var view: RegisterListViewProtocol? { get set }
+    var wireframe: RegisterListWireframeProtocol? { get set }
+    var interactor: RegisterListInteractorInputProtocol? { get set }
+    
+    //VIEW -> PRESENTER
+    func viewDidLoad()
+    func showRegisterDetail(forRegister register: Registro)
+    
+}
+
+protocol RegisterListInteractorInputProtocol: class {
+    
+    //La variable es de tipo HotelListInteractorOutputProtocol porque es la salida del interactor.
+    var presenter: RegisterListInteractorOutputProtocol? { get set }
+    
+    var remoteDataManager: RegisterListRemoteDataManagerInputProtocol? { get set }
+    
+    //PRESENTER -> INTERACTOR
+    func retrieveRegisterList()
+}
+
+protocol RegisterListInteractorOutputProtocol: class {
+    //INTERACTOR -> PRESENTER
+    func didRetrieveRegisters(_ registers: [Registro])
+    func onError()
+}
+
+protocol RegisterListWireframeProtocol: class {
+    
+    //Función donde se conectan todos los modulos.
+    static  func createRegisterListModule() -> UIViewController
+    
+    //PRESENTER -> WIREFRAME
+    func presenterRegisterDetailScreen(from view: RegisterListViewProtocol, forRegister register: Registro)
+    
+}
+
+protocol RegisterListRemoteDataManagerInputProtocol: class {
+    var remoteRequestHandler: RegisterListRemoteDataManagerOutputProtocol? {get set}
+    
+    //INTERACTOR -> REMOTEDATAMANAGER
+    func retrieveRegisterList()
+}
+
+protocol RegisterListRemoteDataManagerOutputProtocol: class {
+    
+    //REMOTEDATAMANAGER -> INTERACTOR
+    func onRegistersRetrieved(_ registers: [Registro])
+    func onError()
+    
+}
---
+++ parqueadero_ios/parqueadero_ios/RegisterList/View/RegisterListViewViewController.swift
@@ -0,0 +1,96 @@
+//
+//  RegisterListViewViewController.swift
+//  parqueadero_ios
+//
+//  Created by Juan Sebastián Flórez Saavedra - Ceiba Software on 2/1/18.
+//  Copyright © 2018 Juan Sebastián Flórez Saavedra - Ceiba Software. All rights reserved.
+//
+
+import UIKit
+import PKHUD
+
+class RegisterListViewViewController: UIViewController {
+
+    //View variables
+    @IBOutlet weak var registerTableView: UITableView!
+    
+    //Module variables
+    var presenter: RegisterListPresenterProtocol?
+    
+    //Other variables
+    var registerList: [Registro] = []
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //Solicita al presentador que cargue la vista.
+        presenter?.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+
+//MARK: - RegisterListViewProtocol
+
+extension RegisterListViewViewController: RegisterListViewProtocol{
+    
+    //Función para mostrar los registros.
+    func showRegister(with registers: [Registro]) {
+       self.registerList = registers
+        registerTableView.reloadData()
+    }
+    
+    func showError() {
+        HUD.flash(.label("Internet not connected"), delay: 2.0)
+    }
+    
+    func showLoading() {
+        HUD.show(.progress)
+    }
+    
+    func hideLoading() {
+        HUD.hide()
+    }
+    
+    
+}
+
+
+//MARK: - TableViewDataSource and TableViewDelegate
+
+extension RegisterListViewViewController: UITableViewDataSource, UITableViewDelegate {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.registerList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = registerTableView.dequeueReusableCell(withIdentifier: "RegisterCell", for: indexPath)
+        
+        let register = registerList[indexPath.row]
+        return cell
+    }
+}
